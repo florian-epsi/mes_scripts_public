214843345cde3d418895a021407fb7e43d4b1c81
florian-epsi <119449270+florian-epsi@users.noreply.github.com>
2023-02-12 11:23:40 +0100

    Update ubuntu_docker_install.sh
    ajout d'une pause

--- ubuntu_docker_install.sh
+++ ubuntu_docker_install.sh
@@ -11,7 +11,7 @@ apt update
 
 # Installation de Docker
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && wait
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io
