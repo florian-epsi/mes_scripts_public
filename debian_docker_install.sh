fb487e0e28249d728c71d4165d98198fbd47128f
florian-epsi <119449270+florian-epsi@users.noreply.github.com>
2023-02-12 11:23:19 +0100

    Update debian_docker_install.sh
    Ajout d'une pause

--- debian_docker_install.sh
+++ debian_docker_install.sh
@@ -11,7 +11,7 @@ apt update
 
 # Installation de Docker
 apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && wait
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce
